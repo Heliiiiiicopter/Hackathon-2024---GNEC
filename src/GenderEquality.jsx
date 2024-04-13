55722513efe51b4f76fb97040e030ccd6133b148
Heliiiiiicopter <gajh06@yahoo.com>
2024-04-13 14:17:33 -0700

    d

--- src/GenderEquality.jsx
+++ src/GenderEquality.jsx
@@ -12,9 +12,13 @@ function GenderEquality() {
       
       <h3> 
         This goal aims to achieve gender equality between women and men in the world. The UN has reached many
-        milestones in their path of equality in the world. They aim to 
+        milestones in their path of equality in the world. So far, according to the UN SDG website, the goal of 
+        gender equality cannot be achieved by 2030, and would take almost 300 years to acheive complete equality.
+        The UN has 9 targets for gender equality, and so far, none of them have been met. 
       </h3>
       
+      <h4> Target 5.1 </h4>
+
     </>
   )
 }
