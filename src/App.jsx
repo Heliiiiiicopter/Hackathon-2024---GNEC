86eb137658337cc1a7953f02bb429cf3f90a5d6c
Heliiiiiicopter <gajh06@yahoo.com>
2024-04-13 13:28:12 -0700

    jhh

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { navigate } from 'react-router-dom'
+
 import './App.css'
 
 function App() {
@@ -10,17 +10,18 @@ function App() {
 
       <h1> United Nations Sustainable Development Goals</h1>
 
-      <table> 
-        <tr>
-          <td> 
+      <div className="summary">
             The goal of the United Nations Sustainable Development Goals or UN SDGs is to reduce the disparities between countries around the world.
-            Specfically between developing and developed countries in various regions around the world. There are 193 members of the United Nations, with all of them pledging to 
-          </td>
-        </tr>
-      </table>
+            Specfically between developing and developed countries in various regions around the world. There are 193 members of the United Nations, 
+            with all of them pledging to achieve all the SDGs by 2030. 
+            <br></br>
+            There are 17 Sustainable Development goals, some trying to end poverty or world hunger. Others trying to improve Literacy Rates and Gender Equality. 
+            And some trying to increase safety and health. 
+        
+          </div>
 
     <div className="goals">
-    <div className="genderEquality" onClick={() => {navigate()}}>
+    <div className="genderEquality">
         Gender Equality
       </div>
       <div className="environment?">
---
+++ src/GenderEquality.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function GenderEquality() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <h1>Goal 5: Gender Equality</h1>
+      
+      <h3> 
+        This goal aims to achieve gender equality between women and men in the world. The UN has reached many
+        milestones in their path of equality in the world. They aim to 
+      </h3>
+      
+    </>
+  )
+}
+
+export default GenderEquality
+
