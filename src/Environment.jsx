37e1cb5369997ae1f2b784211b341696541855bb
Heliiiiiicopter <gajh06@yahoo.com>
2024-04-20 21:59:41 -0700

    ehhehehehe

--- src/Environment.jsx
+++ src/Environment.jsx
@@ -17,8 +17,20 @@ const Environment = () => {
     <p className="p1"> Natural Disasters happen in every country on Earth. Whether it be the East Coast of the US with hurricanes,
      or Earthquakes in China. Many countries are unable to adequately come back from natural disasters. The Indian Ocean Tsunami hit
      countless countries in South and South East Asia, including Indonesia, but also in, India, Malaysia, Singapore, Sri Lanka, and 
-     Thailand. It has taken countless years to rebuild </p>
-    <p></p>
+     Thailand. It has taken countless years to rebuild from the damage. Cities are demolished, lives are lost, and families are ruined 
+     due to unadequate preparation to climate disasters.</p>
+
+     <p>Solutions: <br></br><br></br>
+
+     Policies to lead people after and during disasters. Policies to help locate and help the missing and injured would greatly reduce 
+     deaths. Policies to ensure the quick building of hospitals to treat the injured. So far the amount of injuries and deaths due to 
+     natural disasters has decreased over the last 10 years. Average mortality due to disasters has decreased, but the amount affected 
+     has increased. Many countries have adopted policies or strategies to help with the prevention and managing of natural disasters.
+     </p>
+
+    <h3>Target 2</h3>    
+    <pre>Adopt measures to prevent or slow climate change </pre>
+    <p className="p1"></p>
     <div className="navButtons">
     <button onClick={() => navigate("/")}>Home</button>
     <button onClick={() => navigate("/genderequality")}>Gender Equality</button>
--- src/GenderEquality.jsx
+++ src/GenderEquality.jsx
@@ -31,7 +31,7 @@ function GenderEquality() {
         parental permission. 
       </p>
 
-      <p> Solutions: <br></br>
+      <p> Solutions: <br></br><br></br>
       Implement laws protecting womens rights and their ability to make their own decisions regarding their lives. 
       Many countries have laws regarding domestic abuse, rape, wage gaps, and discrmination. 
       <br></br>
